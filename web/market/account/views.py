69ba4d3f5d67d3e17a98870d7abbb7f9af9b738d
FX2One <dfoxik@gmail.com>
2022-08-05 00:46:31 +0200

    register page

--- web/market/account/views.py
+++ web/market/account/views.py
@@ -1,4 +1,9 @@
 from flask import render_template, redirect, url_for, flash, request, Blueprint
+from market.account.forms import RegisterForm, LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from market.models import User, Item
+from market import db
+from ..email import send_email
 
 account_bp = Blueprint('account', __name__, template_folder='templates')
 
@@ -7,4 +12,26 @@ account_bp = Blueprint('account', __name__, template_folder='templates')
 def home_page():
     return render_template('home.html')
 
+@account_bp.route('/register', methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        create_user = User(
+            username=form.username.data,
+            email_address=form.email_address.data,
+            password=form.password.data)
+        db.session.add(create_user)
+        db.session.commit()
+        login_user(create_user)
+        token = create_user.generate_auth_token()
+        confirm_link = url_for('account.confirm', token=token, _external=True)
+        send_email(create_user.email_address, 'Confirm your account', 'confirm', user=create_user, token=token,
+                   confirm_link=confirm_link)
+        flash('You are now registered and successfully logged in', category='success')
+        flash('confirmation email has been sent', category='success')
+        return redirect(url_for('items.market_page'))
 
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+    return render_template('register.html', form=form)

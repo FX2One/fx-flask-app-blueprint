a05a482747bacebea5d31429fdd753dc6dcdfc98
FX2One <dfoxik@gmail.com>
2022-08-05 00:44:52 +0200

    home view and basic templating

--- web/market/account/views.py
+++ web/market/account/views.py
@@ -1,3 +1,10 @@
 from flask import render_template, redirect, url_for, flash, request, Blueprint
 
 account_bp = Blueprint('account', __name__, template_folder='templates')
+
+@account_bp.route('/')
+@account_bp.route('/home')
+def home_page():
+    return render_template('home.html')
+
+

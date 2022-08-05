ea595af26e8b9a277a1ec4db1a67dcd1d9f329d1
FX2One <dfoxik@gmail.com>
2022-08-05 13:27:35 +0200

    send mail functionality

---
+++ web/market/email.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from flask_mail import Message
+from market import mail
+from flask import current_app
+
+
+def send_email(recipient, subject, template, **kwargs):
+    app = current_app._get_current_object()
+    with app.app_context():
+        msg = Message(
+            subject = app.config['EMAIL_SUBJECT_PREFIX'] + ' ' + subject,
+            sender=app.config['EMAIL_SENDER'],
+            recipients=[recipient])
+        msg.html = render_template(template + '.html', **kwargs)
+        msg.body = render_template(template + '.txt', **kwargs)
+        mail.send(msg)

e9e9f6279e8de5eb34875c30fb4f0177510e6c4c
FX2One <dfoxik@gmail.com>
2022-08-05 22:59:07 +0200

    forms for notes

---
+++ web/market/notes/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField,
+from wtforms.validators import InputRequired, Length, EqualTo
+
+class NoteForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired(),Length(10, 60)])
+
+    notation = StringField('Note', validators=[InputRequired(),
+                                               Length(10, 999)])
+
+    submit = SubmitField('Submit')
+
+
+class EditNoteForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired(),Length(10, 60)])
+
+    notation = StringField('Note', validators=[InputRequired(),
+                                               Length(10, 999)])
+
+    submit = SubmitField('Submit')

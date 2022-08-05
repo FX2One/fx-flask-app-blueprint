1eaf8ade355622ace666f0aefce3e60c91c78154
FX2One <dfoxik@gmail.com>
2022-08-05 23:49:17 +0200

    post forms added

---
+++ web/market/posts/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, SubmitField, TextAreaField
+from wtforms.validators import InputRequired, Length
+
+
+class AddPostForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired(),Length(10, 60)])
+
+    post = TextAreaField('Post Section', validators=[InputRequired(),
+                                                     Length(10, 2000)])
+
+    submit = SubmitField('Submit')
+
+
+class EditPostForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired(),Length(10, 60)])
+
+    post = TextAreaField('Post Section', validators=[InputRequired(),
+                                                     Length(10, 2000)])
+
+    submit = SubmitField('Submit')

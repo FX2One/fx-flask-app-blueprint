67bb2b1181b148de891601e6d3b89c16b68e465e
FX2One <dfoxik@gmail.com>
2022-08-06 23:17:39 +0200

    forms modules check

--- web/market/notes/forms.py
+++ web/market/notes/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField,
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired, Length
 
 class NoteForm(FlaskForm):
     title = StringField('Title', validators=[InputRequired(),Length(10, 60)])
